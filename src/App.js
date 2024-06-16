3fc969dc5c1c939e864e0d9ead856bd902df9257
Iarley-01 <yarleyalves2016@gmail.com>
2024-06-15 22:56:04 -0300

    modificando elementos do array de personagens e aplicando famílias de fonte

--- src/App.js
+++ src/App.js
@@ -57,88 +57,56 @@ function App() {
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'JULIANA AMOASEI',
-      cargo: 'Desenvolvedora de software e instrutora',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      nome: 'Kumiko Oumae',
+      cargo: 'Eufonio',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/6/68/Kumiko_Oumae.jpg',
       secao: secoes[0].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'DANIEL ARTINE',
-      cargo: 'Engenheiro de Software na Stone Age',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      nome: 'Asuka Tanaka',
+      cargo: 'Eufonio',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/b/bb/Asuka_Tanaka.jpg',
       secao: secoes[0].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'GUILHERME LIMA',
-      cargo: 'Desenvolvedor Python e JavaScript na Alura',
-      imagem: '        https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      nome: 'Natsuki Nakagawa',
+      cargo: 'Eufonio',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/d/d6/Natsuki_Nakagawa.jpg',
       secao: secoes[0].nome
     },
     {
       id: uuidv4(),
-      nome: 'PAULO SILVEIRA',
-      cargo: 'Hipster e CEO da Alura',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      nome: 'Sapphire Kawashima',
+      cargo: 'Contrabaixo',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/f/f2/Sapphire_Kawashima.jpg',
       secao: secoes[0].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'JULIANA AMOASEI',
-      cargo: 'Desenvolvedora de software e instrutora',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      nome: 'Mizore Yoroizuka',
+      cargo: 'Oboé',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/5/5e/Mizore_Yoroizuka.jpg',
       secao: secoes[1].nome
     },
     {
       id: uuidv4(),
-      nome: 'DANIEL ARTINE',
-      cargo: 'Engenheiro de Software na Stone Age',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
-      secao: secoes[1].nome
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: 'GUILHERME LIMA',
-      cargo: 'Desenvolvedor Python e JavaScript na Alura',
-      imagem: '        https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      nome: 'Raina Kitamura',
+      cargo: 'Baixão',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/1/19/Raina_Kitamura.jpg',
       secao: secoes[1].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'PAULO SILVEIRA',
-      cargo: 'Hipster e CEO da Alura',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      nome: 'Mikino Oka',
+      cargo: 'Baixão',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/b/b6/Mikino_Oka.jpg',
       secao: secoes[1].nome
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: 'JULIANA AMOASEI',
-      cargo: 'Desenvolvedora de software e instrutora',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
-      secao: secoes[2].nome
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: 'DANIEL ARTINE',
-      cargo: 'Engenheiro de Software na Stone Age',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
-      secao: secoes[2].nome
-    },
-    {
-      id: uuidv4(),
-      favorito: false,
-      nome: 'GUILHERME LIMA',
-      cargo: 'Desenvolvedor Python e JavaScript na Alura',
-      imagem: '        https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
-      secao: secoes[2].nome
     },
     {
       id: uuidv4(),
@@ -146,66 +114,98 @@ function App() {
       nome: 'PAULO SILVEIRA',
       cargo: 'Hipster e CEO da Alura',
       imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      secao: secoes[1].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Hideri Noguchi',
+      cargo: '1⁰ Trombone',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/2/2f/Hideri_Noguchi.jpg',
       secao: secoes[2].nome
     },
     {
       id: uuidv4(),
-      nome: 'JULIANA AMOASEI',
-      cargo: 'Desenvolvedora de software e instrutora',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      favorito: false,
+      nome: 'Maki Akamatsu',
+      cargo: '1⁰ Trombone',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/7/70/Maki_Akamatsu_Character_Intro.jpg',
+      secao: secoes[2].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Mei Taura',
+      cargo: '2⁰ Trombone',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/c/cc/Mei_Taura.jpg',
+      secao: secoes[2].nome
+    },
+    {
+      id: uuidv4(),
+      favorito: false,
+      nome: 'Shuuichi Tsukamoto',
+      cargo: '2⁰ Trombone',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/8/82/Shuuichi_Tsukamoto.jpg',
+      secao: secoes[2].nome
+    },
+    {
+      id: uuidv4(),
+      nome: 'Juri Sawada',
+      cargo: '1ª Trompa',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/0/0f/Juri_Sawada.jpg',
       secao: secoes[3].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'DANIEL ARTINE',
-      cargo: 'Engenheiro de Software na Stone Age',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      nome: 'Hiro Kahashi',
+      cargo: '2ª Trompa',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/0/02/Hiro_Kahashi1.jpg',
       secao: secoes[3].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'GUILHERME LIMA',
-      cargo: 'Desenvolvedor Python e JavaScript na Alura',
-      imagem: '        https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      nome: 'Michiyo Morimoto',
+      cargo: '3ª Trompa',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/a/ab/Michiyo_Morimoto_Character_Intro.jpg',
       secao: secoes[3].nome
     },
     {
       id: uuidv4(),
-      nome: 'PAULO SILVEIRA',
-      cargo: 'Hipster e CEO da Alura',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      nome: 'Miru Kishibe',
+      cargo: '4ª Trompa',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/5/54/Miru_Kishibe.jpg',
       secao: secoes[3].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'JULIANA AMOASEI',
-      cargo: 'Desenvolvedora de software e instrutora',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/juliana-amoasei.1647533644.jpeg',
+      nome: 'Kaori Nakaseko',
+      cargo: '1⁰ Trompete',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/a/aa/Kaori_Nakaseko.jpg',
       secao: secoes[4].nome
     },
     {
       id: uuidv4(),
-      nome: 'DANIEL ARTINE',
-      cargo: 'Engenheiro de Software na Stone Age',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/daniel-artine.1647533644.jpeg',
+      nome: 'Reina Kousaka',
+      cargo: '1⁰ Trompete',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/7/70/Reina_Kousaka.jpg',
       secao: secoes[4].nome
     },
     {
       id: uuidv4(),
       favorito: false,
-      nome: 'GUILHERME LIMA',
-      cargo: 'Desenvolvedor Python e JavaScript na Alura',
-      imagem: '        https://www.alura.com.br/assets/img/lideres/guilherme-lima.1647533644.jpeg',
+      nome: 'Yuuko Yoshikawa',
+      cargo: '2⁰ Trompete',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/5/50/Yuuko_Yoshikawa.jpg',
       secao: secoes[4].nome
     },
     {
       id: uuidv4(),
-      nome: 'PAULO SILVEIRA',
-      cargo: 'Hipster e CEO da Alura',
-      imagem: 'https://www.alura.com.br/assets/img/lideres/paulo-silveira.1647533644.jpeg',
+      nome: 'Sana Kasano',
+      cargo: '3⁰ Trompete',
+      imagem: 'https://static.wikia.nocookie.net/hibike-euphonium/images/f/fe/Sana_Kasano.jpg',
       secao: secoes[4].nome
     },
     {
